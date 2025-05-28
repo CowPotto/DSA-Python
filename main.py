b3fbe299bd17c867149b35a24275c94f72059a49
CowPotto <cclemoncow69@gmail.com>
2025-05-28 18:01:33 +0700

    adding shit

--- main.py
+++ main.py
@@ -1,16 +1,65 @@
-# This is a sample Python script.
+class SinglyNode:
+    def __init__(self, val, next = None):
+        self.val = val
+        self.next = next
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def __str__(self, val):
+        return str(self.val)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def display(head):
+    curr = head
+    elements = []
+    while curr:
+        elements.append(str(curr.val))
+        curr = curr.next
+    print(' -> '.join(elements))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def value_check(head, target):
+    curr = head
+    while curr:
+        if curr.val == target:
+            return True
+        curr = curr.next
+    return False
+
+def length_of_list(head):
+    curr = head
+    length = 0
+    while curr:
+        if curr.val != 0:
+            length += 1
+            curr = curr.next
+    return length
+
+def delete_node(head, pos):
+    curr = head
+    prev = None
+    if pos == 0:
+        head = curr.next
+        return head
+
+    for i in range(0, pos):
+        prev = curr
+        curr = curr.next
+
+    prev.next = curr.next
+    return head
+
+
+
+
+
+
+if __name__ == "__main__":
+    head = SinglyNode(1)
+    head.next = SinglyNode(2)
+    head.next.next = SinglyNode(3)
+    head.next.next.next = SinglyNode(4)
+    head.next.next.next.next = SinglyNode(5)
+    head = delete_node(head, 4)
+    display(head)
+
+
+
+
