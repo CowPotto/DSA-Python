b773f3a77dae6e57da66a3fd022cb1e467521b98
CowPotto <cclemoncow69@gmail.com>
2025-06-20 16:03:19 +0700

    practice.py changed

--- practice.py
+++ practice.py
@@ -1,6 +1,3 @@
-
-
-
 class Node:
     def __init__(self, data):
         self.data = data
@@ -37,19 +34,63 @@ class SinglyLinkedList:
         self.head = new_node
 
     def delete(self, ddata):
-        curr = self.head
-        if self.head is None:
-            print("Nothing to delete sir")
-            return
-
+        #case 1: the deleted data is the head
         if self.head == ddata:
             self.head = self.head.next
             return
 
+        #case 2: handle empty list
+        if self.head is None:
+            print("The list is empty, so what are you deleting!")
+            return
 
+        #case 3: normal traversal through the list
+        curr = self.head
+        prev = None
+        while curr and curr.data != ddata:
+            prev = curr
+            curr = curr.next
 
+        #case 3.1: number is not in the list
+        if curr is None:
+            print("the number you want to delete is nowhere to be found in the fucking list mate!")
+            return
+        #case 3.2: found the number
+        prev.next = curr.next
+        curr.next = None
 
+    def add_node_at_pos(self, adata, pos):
+        new_node = Node(adata)
 
+        #case 1: list is empty
+        if self.head is None:
+            self.head = new_node
+            return
+
+        #case 2: add to 1st node -> prepend
+        if pos == 0: #0-indexed
+            self.prepend(adata)
+            return
+
+        #case 3: traversal through the list
+        prev = None
+        curr = self.head
+        curr_idx = 0
+        while curr and curr_idx < pos:
+            prev = curr
+            curr = curr.next
+            curr_idx += 1
+
+        if prev is None:
+            print(f"Cannot add at position {pos}. List is empty or position is invalid.")
+            return
+
+        if curr is None and curr_idx < pos:
+            print("the position is beyond the list size")
+            return
+
+        prev.next = new_node
+        new_node.next = curr
 
 
     def display(self):
@@ -67,7 +108,6 @@ if __name__ == "__main__":
     head.append(4)
     head.append(6)
     head.append(8)
-    head.prepend(1)
-    head.prepend(0)
-    head.delete(0)
+    head.add_node_at_pos(1, 2)
+
     head.display()
