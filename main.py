c7d4141a3e51d554ab56c433a69ce725fd2b8e2c
CowPotto <cclemoncow69@gmail.com>
2025-06-24 14:50:37 +0700

    Your mom fat

--- main.py
+++ main.py
@@ -1,102 +1,116 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-class LinkedList:
-    def __init__(self, head = None):
-        self.head = head
+def list_to_llist(arr) -> ListNode:
+    head = ListNode(arr[0])
+    curr = head
+    for i in range(1, len(arr)):
+        curr.next = ListNode(arr[i])
+        curr = curr.next
+    return head
 
-    def append(self, data):
-        #create new node using Node(data)
-        new_node = Node(data)
+def llist_to_list(head) -> list:
+    curr = head
+    arr = []
+    while curr:
+        arr.append(curr.val)
+        curr = curr.next
+    return arr
 
-        #if there is no node yet, the new node is the head
-        if self.head is None:
-            self.head = new_node
-            return
+def llist_display(head) -> None:
+    curr = head
+    elements = []
+    while curr:
+        elements.append(str(curr.val))
+        curr = curr.next
+    print(' -> '.join(elements))
 
-        curr = self.head
-        # traverse through the list until the curr.next = None, means that it is the last node
-        while curr.next:
-            curr = curr.next
-        curr.next = new_node
+class Solution:
+    def addTwoNumbers(self, l1:ListNode, l2:ListNode) -> ListNode:
+        dummy_head = ListNode(0)
+        curr = dummy_head
+        carry = 0
+        while l1 or l2 or carry:
+            if l1 is not None:
+                val1 = l1.val
+            else:
+                val1 = 0
 
-    def prepend(self, data):
-        new_node = Node(data)
-        #the next pointer of new node point to head
-        new_node.next = self.head
-        #set the new node to current head
-        self.head = new_node
+            if l2 is not None:
+                val2 = l2.val
+            else:
+                val2 = 0
 
-    def display(self):
-        curr = self.head
-        if not curr:
-            print("The list is empty!")
-            return
-
-        elements = []
-        while curr:
-            elements.append(str(curr.data))
-            curr = curr.next
-        print(' -> '.join(elements))
-
-    def add_node_after(self, prev_node_data, new_data):
-        if not self.head:
-            print("the list is fucking empty")
-            return
-
-        found_prev_node = False
-        curr = self.head
-        while curr:
-            if curr.data == prev_node_data:
-                found_prev_node = True
-                break
+            sum_val = val1 + val2 + carry
+            digit = sum_val % 10
+            carry = sum_val // 10
+            curr.next = ListNode(digit)
             curr = curr.next
 
-        if not found_prev_node:
-            print(f"there is no {prev_node_data} in the fucking list mate")
-            return
-
-        new_node = Node(new_data)
-        new_node.next = curr.next
-        curr.next = new_node
-
-    def delete_node(self, specific_data):
-        #Case 1: The number is nowhere to find in the list
-        if not self.head:
-            print("This fuccking list is empty, again!")
-            return
-
-        #Case 2: the number is the head node
-        if self.head.data == specific_data:
-            self.head = self.head.next
-            return
-
-        #Case 3: normal traversal to delete specific node using prev and curr technique
-        prev = None
-        curr = self.head
-        while curr and curr.data != specific_data:
-            prev = curr
-            curr = curr.next
-
-        if curr is None:
-            print("you cannot delete a number that is not in the list you dumb fuck!")
-            return
-
-        prev.next = curr.next
-        curr.next = None
-
-#=======================================================================================================================
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        return dummy_head.next
 
 if __name__ == "__main__":
-    head = LinkedList()
-    head.append(5)
-    head.append(7)
-    head.append(9)
-    head.prepend(3)
-    head.add_node_after(9, 4)
-    head.delete_node(4)
-    head.delete_node(5)
+    # Correction: Instantiate the Solution class
+    s = Solution()
 
-    head.display()
+    print("--- Test Case 1 ---")
+    list_1 = [1, 2, 3] # Represents 321
+    list_2 = [4, 5, 6] # Represents 654
+    llist_1 = list_to_llist(list_1)
+    llist_2 = list_to_llist(list_2)
+    print("Input List 1:", list_1)
+    print("Input List 2:", list_2)
+    print("Linked List 1 (visual): ", end="")
+    llist_display(llist_1)
+    print("Linked List 2 (visual): ", end="")
+    llist_display(llist_2)
+
+    result_llist = s.addTwoNumbers(llist_1, llist_2)
+    print("Result Linked List (visual): ", end="")
+    llist_display(result_llist)
+    print("Result as List:", llist_to_list(result_llist)) # Expected: [5, 7, 9] (975)
+    print("-" * 20)
+
+    print("--- Test Case 2: Different Lengths with Carry ---")
+    list_3 = [9, 9] # Represents 99
+    list_4 = [9, 9, 9] # Represents 999
+    llist_3 = list_to_llist(list_3)
+    llist_4 = list_to_llist(list_4)
+    print("Input List 1:", list_3)
+    print("Input List 2:", list_4)
+    print("Linked List 1 (visual): ", end="")
+    llist_display(llist_3)
+    print("Linked List 2 (visual): ", end="")
+    llist_display(llist_4)
+
+    result_llist_2 = s.addTwoNumbers(llist_3, llist_4)
+    print("Result Linked List (visual): ", end="")
+    llist_display(result_llist_2)
+    print("Result as List:", llist_to_list(result_llist_2)) # Expected: [8, 9, 0, 1] (1098)
+    print("-" * 20)
+
+    print("--- Test Case 3: Final Carry ---")
+    list_5 = [9] # Represents 9
+    list_6 = [9] # Represents 9
+    llist_5 = list_to_llist(list_5)
+    llist_6 = list_to_llist(list_6)
+    print("Input List 1:", list_5)
+    print("Input List 2:", list_6)
+    print("Linked List 1 (visual): ", end="")
+    llist_display(llist_5)
+    print("Linked List 2 (visual): ", end="")
+    llist_display(llist_6)
+
+    result_llist_3 = s.addTwoNumbers(llist_5, llist_6)
+    print("Result Linked List (visual): ", end="")
+    llist_display(result_llist_3)
+    print("Result as List:", llist_to_list(result_llist_3)) # Expected: [8, 1] (18)
+    print("-" * 20)
+
+
